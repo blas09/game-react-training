430103420c56d4a7f47128e94ca7aed437e154e8
Blas Amarilla <bamarilla@reputationinstitute.com>
2020-09-04 19:34:03 -0300

    fixed image

--- src/components/Game/GamePanel/CardPanel/Card.js
+++ src/components/Game/GamePanel/CardPanel/Card.js
@@ -9,7 +9,7 @@ const Card = () => {
         ? <Loader />
         :   <>
             <figure className="image is-4by3">
-                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="" />
             </figure>
         </>;
 
--- src/components/Game/GamePanel/UserPanel/UserPanel.js
+++ src/components/Game/GamePanel/UserPanel/UserPanel.js
@@ -2,8 +2,6 @@ import React from "react";
 import Loader from "../../../Loader";
 
 const UserPanel = ({user}) => {
-    console.log(user);
-
     const userData = null === user
         ? <Loader />
         :   <>
@@ -18,20 +16,20 @@ const UserPanel = ({user}) => {
                 <p className="subtitle">{user.shield}</p>
             </>;
 
-    const userImage = null === user
-        ? <Loader />
-        :   <>
-                <figure className="image is-128x128" style={{overflow:'hidden'}}>
-                    <img src={null === user.image ? 'https://bulma.io/images/placeholders/128x128.png' : user.image} />
-                </figure>
-            </>;
+    const userImage = null === user || null === user.image
+        ? 'https://bulma.io/images/placeholders/128x128.png'
+        : user.image;
 
     return (
         <div className="columns is-mobile">
             <div className="column is-7">
                 <div className="box">
                     <article className="media">
-                        <div className="media-left">{userImage}</div>
+                        <div className="media-left">
+                            <figure className="image is-128x128" style={{overflow:'hidden'}}>
+                                <img src={userImage} alt="" />
+                            </figure>
+                        </div>
                         <div className="media-content">{userData}</div>
                     </article>
                 </div>
